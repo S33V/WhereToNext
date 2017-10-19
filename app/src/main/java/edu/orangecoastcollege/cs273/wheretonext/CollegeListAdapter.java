7be93b5a24a2de1ae6ebe9894e5bbdd8ab2fca73
Severiano Badajoz <sevebadajoz@gmail.com>
2017-10-18 23:38:04 -0700

    Begin work on CollegeListAdapter.java and fixing inflater

--- app/src/main/java/edu/orangecoastcollege/cs273/wheretonext/CollegeListAdapter.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/wheretonext/CollegeListAdapter.java
@@ -2,14 +2,23 @@ package edu.orangecoastcollege.cs273.wheretonext;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.RatingBar;
+import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Helper class to provide custom adapter for the <code>College</code> list.
@@ -21,7 +30,7 @@ public class CollegeListAdapter extends ArrayAdapter<College> {
     private int mResourceId;
 
     private RatingBar mCollegeListRatingBar;
-
+    private TextView collegeListNameTextView;
 
 
     /**
@@ -59,6 +68,18 @@ public class CollegeListAdapter extends ArrayAdapter<College> {
         mCollegeListRatingBar = (RatingBar) view.findViewById(R.id.collegeListRatingBar);
         mCollegeListRatingBar.setRating((float) selectedCollege.getRating());
         mCollegeListRatingBar.setTag(selectedCollege);
+
+        AssetManager am = mContext.getAssets();
+        try {
+            InputStream stream = am.open(selectedCollege.getImageName());
+            Drawable event = Drawable.createFromStream(stream, selectedCollege.getImageName());
+            ((ImageView)view.findViewById(R.id.collegeListImageView)).setImageDrawable(event);
+            Log.i(TAG, "getView: found Image " + event.toString());
+        } catch (IOException e) {
+            Log.e(TAG, "getView: ", e);
+        }
+
+        ((TextView)view.findViewById(R.id.collegeListNameTextView)).setText(selectedCollege.getName());
 
         return view;
     }
